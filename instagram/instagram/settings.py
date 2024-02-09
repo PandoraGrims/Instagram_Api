2d604a437022ed05de2503a585979ee3396b0f72
Pandora <nimfanefor@gmail.com>
2024-02-09 17:21:46 +0600

    Сделал первичные настройки проекта

--- instagram/instagram/settings.py
+++ instagram/instagram/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'webapp',
+    'accounts',
+    'api_v1',
 ]
 
 MIDDLEWARE = [
@@ -111,13 +114,21 @@ USE_I18N = True
 
 USE_TZ = True
 
+AUTH_USER_MODEL = 'accounts.User'
+
+LOGIN_URL = "accounts:login"
+LOGIN_REDIRECT_URL = "webapp:posts_list"
+LOGOUT_REDIRECT_URL = "webapp:posts_list"
+
+MEDIA_ROOT = Path.joinpath(BASE_DIR, 'uploads')
+MEDIA_URL = '/media/'
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
